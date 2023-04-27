b7c28726693a11ccbb98cbd089474333367a69b6
Winter Lu <1352252075@qq.com>
2023-04-27 09:52:29 +0800

    :zap: Improve Code Performance

--- code/02_train-wordvec.R
+++ code/02_train-wordvec.R
@@ -11,64 +11,74 @@ pacman::p_load(jiebaR)
 pacman::p_load(arrow)
 pacman::p_load(dplyr)
 pacman::p_load(stringr)
+pacman::p_load(here)
 
+# Locate the project
+i_am("code/02_train-wordvec.R")
 
 # 1. Split Tokens ---------------------------------------------------------
 ## Split tokens using jiebaR
 word_segment <- worker(stop_word = "data/baidu_stopwords.txt")
 
 ## Conduct splitting by year
-res_list = list()
-for (year in 2016:2022) {
-  print(paste("Now process year", year))
+res_vec = c()
+for (year in c(2010:2022)) {
+  message(paste("Now process year", year), "tokens...")
   
-  print("=> Collect text...")
+  message("=> Collect text...")
   path = paste0("data/HuDongE/Year=", year)
   hde_arrow <- open_dataset(sources = path)
   hde_text <- hde_arrow |> 
     select(提问内容, 回复内容) |> 
     collect()
   
-  print("=> Combine text...")
+  message("=> Combine text...")
   size <- nrow(hde_text)
   hde_text <- str_flatten(
     string = Reduce(paste, hde_text[1:size, ])
   )
   
-  print("=> Split tokens...")
+  message("=> Split tokens...")
   tokens <- segment(
     code = str_remove_all(hde_text, pattern = "[0-9]"),
     jiebar = word_segment
   )
   
-  res_list <- append(res_list, unlist(tokens))
+  message("=> Combine tokens...")
+  res_vec <- c(res_vec, tokens)
   rm(path, hde_arrow, hde_text, size, tokens)
 }
-res_list <- list(unlist(res_list))
 
 
 # 2. Train Wordvec --------------------------------------------------------
 ## Build corpus
-it = itoken(res_list, progressbar = TRUE)
+it = itoken(list(res_vec), progressbar = TRUE)
 vocab = create_vocabulary(it)
-vocab = prune_vocabulary(vocab, term_count_min = 4L)
+vocab = prune_vocabulary(vocab, term_count_min = 10L)
 
 ## Calculate co-occurrence matrix
+message("\nNow build co-occurrence matrix...")
 vectorizer = vocab_vectorizer(vocab)
 tcm = create_tcm(it, vectorizer, skip_grams_window = 5L)
 
-saveRDS(object = res_list, file = "processed/tokens16-22.rds")
-rm(res_list)
+saveRDS(
+  object = res_vec, 
+  file = "processed/HDE_tokens_10-22.rds"
+)
+rm(res_vec)
 
 ## Generate wordvec
-glove = GlobalVectors$new(rank = 50, x_max = 10)
-wv_main = glove$fit_transform(tcm, n_iter = 10, convergence_tol = 0.01, n_threads = 8)
-
+message("\nNow train word vectors...")
+glove = GlobalVectors$new(rank = 128, x_max = 10)
+wv_main = glove$fit_transform(
+  tcm, n_iter = 30, 
+  convergence_tol = 0.01, n_threads = 8
+)
 wv_context = glove$components
 word_vectors = wv_main + t(wv_context)
 
 ## Test wordvec 
-find_similar_words <- function(keyword) {
+find_similar_words <- function(keyword, num = 10) {
   target_word = word_vectors[keyword, , drop = FALSE]
   cos_sim = sim2(
     x = word_vectors, 
@@ -76,13 +86,19 @@ find_similar_words <- function(keyword) {
     method = "cosine", 
     norm = "l2"
   )
-  head(sort(cos_sim[, 1], decreasing = TRUE), 5)
+  head(sort(cos_sim[, 1], decreasing = TRUE), num)
 }
 find_similar_words(keyword = "一年")
 
 ## Save wordvec as rds file
-saveRDS(object = tcm, file = "processed/tcm16-22.rds")
-saveRDS(object = word_vectors, file = "processed/word_vectors16-22.rds")
+saveRDS(
+  object = tcm, 
+  file = "processed/HDE_tcm_10-22.rds"
+)
+saveRDS(
+  object = word_vectors, 
+  file = "processed/HDE_wordvec128_10-22.rds"
+)
 
 
 ### EOF
